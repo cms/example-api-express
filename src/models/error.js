36192fb0d221a2b70bc8b2b307cba7702ce2872f
Christian C. Salvado <cmsalvado@vonap.com>
2018-10-07 21:54:55 -0600

    Add more precise description of models

--- src/models/error.js
+++ src/models/error.js
@@ -2,9 +2,13 @@ import SchemaObject from "schema-object";
 const NotEmptyString = { type: String, minLength: 1 };
 
 const Error = new SchemaObject({
-  errorType: { type: NotEmptyString, enum: ["COM", "SEG", "MSJ"] },
-  code: String,
-  description: String
+  errorType: {
+    type: String,
+    enum: ["COM", "SEG", "MSJ"],
+    required: true
+  },
+  code: { type: NotEmptyString, required: true },
+  description: { type: NotEmptyString, required: true }
 });
 
 export default Error;
--- src/models/responseValidations.js
+++ src/models/responseValidations.js
@@ -2,7 +2,7 @@ import SchemaObject from "schema-object";
 
 const ResponseValidations = new SchemaObject({
   description: String,
-  validationResult: Boolean
+  validationResult: { type: Boolean, required: true }
 });
 
 export default ResponseValidations;
