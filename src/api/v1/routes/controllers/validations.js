a709cd8ceb6cb08072ea239e2f2849ae08432973
Christian C. Salvado <cmsalvado@vonap.com>
2018-11-26 21:33:04 -0600

    Read get parameters from first endpoint test

--- src/api/v1/routes/controllers/validations.js
+++ src/api/v1/routes/controllers/validations.js
@@ -1,4 +1,14 @@
+import queryString from "query-string";
+
 export default (req, res, next) => {
+  const { authorization } = req.headers;
+  const {
+    query: { customerId, customerIdType, endPoint }
+  } = queryString.parseUrl(req.url);
+
+  console.log(customerId, customerIdType, endPoint);
+  console.log(authorization);
   console.log("Validations");
+
   res.json({ message: "Welcome to the CBS v1 API Validations endpoint" });
 };
